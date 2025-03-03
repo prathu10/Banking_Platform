1c0425db5f24d1d863ff18106d29426b8d8769e0
Prathu <prathamesh4402@gmail.com>
2025-03-02 17:27:17 -0700

    header box, and root page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import TotalBalanceBox from "@/components/TotalBalanceBox"
+import HeaderBox from "@/components/ui/HeaderBox"
 
 const Home = () => {
+  const loggedIn = {firstName: "Prathamesh"}
   return (
-    <div>
-      
-    </div>
+    <section className='home'>
+      <div className='home-content'>
+        <div className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."/>
+
+            <TotalBalanceBox 
+              accounts={[]}
+              totalBanks={1}
+              totalCurrentBalance={1250.35}/>
+        </div>
+
+      </div>
+    </section>
   )
 }
 
---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TotalBalanceBox = ({
+    accounts = [], totalBanks, totalCurrentBalance
+}: TotalBalanceBoxProps) => {
+  return (
+    <section className='total=balance'>
+       <div className='total-balance-chart'>
+
+        </div> 
+    </section>
+  )
+}
+
+export default TotalBalanceBox
